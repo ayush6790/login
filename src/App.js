00d9fc56016ecd63728db8c2331c19bbf6298cf7
ayush6790 <ayush6790@gmail.com>
2023-07-21 14:32:02 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/cntdn" element={<Home />}/>
+        {/* <Route path="/cntdn" element={<Home />}/> */}
         <Route path="/login" element={< LoginPage />}/>            
         <Route path="/welcome" element={< WelcomeUser />}/>            
                     
